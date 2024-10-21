08f3b9349c69530c6943a20ba00970c884e43b08
MerelDej <146180376+MerelDej@users.noreply.github.com>
2024-10-21 08:52:15 +0200

    Initial commit

---
+++ PeopleManager.Ui.Mvc/Controllers/PeopleController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PeopleManager.Ui.Mvc.Core;
+using PeopleManager.Ui.Mvc.Models;
+
+namespace PeopleManager.Ui.Mvc.Controllers
+{
+    public class PeopleController : Controller
+    {
+
+        private readonly Database _database;
+
+        public PeopleController(Database database)
+        {
+            _database = database;
+        }
+
+        public IActionResult Index()
+        {
+            var people = _database.People;
+
+            return View(people);
+        }
+    }
+}
