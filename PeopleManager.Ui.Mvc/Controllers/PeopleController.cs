10fb6b2921d05681ce238c8c83bbbac959ac62af
MerelDej <merel.dejaegere@student.vives.be>
2024-11-18 10:45:30 +0100

    integer in delete id

--- PeopleManager.Ui.Mvc/Controllers/PeopleController.cs
+++ PeopleManager.Ui.Mvc/Controllers/PeopleController.cs
@@ -75,7 +75,7 @@ namespace PeopleManager.Ui.Mvc.Controllers
         }
 
         [HttpPost]
-        [Route("People/Delete/{id}")]
+        [Route("People/Delete/{id:int}")]
         [ValidateAntiForgeryToken]
         public IActionResult DeleteConfirmed([FromRoute] int id)
         {
